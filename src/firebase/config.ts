8d44d1635ef79e21868c9804a24ff8ad860fc44e
Tasir Mohtasim <mohtasimmoktadirtasir@gmail.com>
2026-02-09 07:23:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7798432744-26c29",
+  "appId": "1:21509883752:web:7dcccfc9083d47ba7f38dd",
+  "apiKey": "AIzaSyBP3ERGt95S9o7qjksRTdZpU5ZkShRO4D0",
+  "authDomain": "studio-7798432744-26c29.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "21509883752"
+};
