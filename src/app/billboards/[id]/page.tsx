a2bd6d1f14e0107646c1802e052dd7936de232fa
Tasir Mohtasim <mohtasimmoktadirtasir@gmail.com>
2026-02-11 08:46:56 +0000

    make this box bigger

--- src/app/billboards/[id]/page.tsx
+++ src/app/billboards/[id]/page.tsx
@@ -69,9 +69,9 @@ export default function BillboardDetailPage() {
             Back to Listings
           </Button>
 
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12">
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 lg:gap-12">
             {/* Image Carousel */}
-            <div className="lg:col-span-2">
+            <div className="lg:col-span-3">
                <Carousel className="w-full" opts={{ loop: true }}>
                     <CarouselContent>
                     {(billboard.images && billboard.images.length > 0) ? (
